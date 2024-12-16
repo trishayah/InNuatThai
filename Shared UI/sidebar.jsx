20fc74bfeefbe1cd017a3e8ac6179fd04bf3cfc1
trishiah-rellon <trishiah.rellon@ctu.edu.ph>
2024-12-16 16:05:41 +0800

    sidebar complete pero di pani ok

--- Shared UI/sidebar.jsx
+++ Shared UI/sidebar.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import logo from "../src/assets/NUAT THAI LOGO.svg";
-import { 
-  MdDashboard, 
-  MdAssignment, 
-  MdInventory, 
+import {
+  MdDashboard,
+  MdAssignment,
+  MdInventory,
   MdDescription,
-  MdArchive, 
+  MdArchive,
   MdLogout,
   MdKeyboardArrowDown,
-  MdKeyboardArrowUp 
+  MdKeyboardArrowUp,
 } from "react-icons/md";
 
 const Sidebar = ({ role }) => {
@@ -20,20 +20,56 @@ const Sidebar = ({ role }) => {
 
   const menuItems = {
     admin: [
-      { name: "Dashboard", url: "/dashboard", icon: MdDashboard},
-      { name: "Request", url: "/request", icon: MdAssignment},
-      { name: "Inventory", url: "/inventory", icon: MdInventory},
+      { name: "Dashboard", url: "/dashboard", icon: MdDashboard },
+      { name: "Request", url: "/request", icon: MdAssignment },
+      { name: "Inventory", url: "/inventory", icon: MdInventory },
       {
         name: "Reports",
         icon: MdDescription,
         isDropdown: true,
         subItems: [
-          { name: "Delivery Instruction Form", route: "/reports/dif"},
-          { name: "Stock Receiving Report", route: "/reports/wsrr"},
-          { name: "Purchase Order", route: "/reports/po"},
+          { name: "Delivery Instruction Form", route: "/reports/dif" },
+          { name: "Stock Receiving Report", route: "/reports/wsrr" },
+          { name: "Purchase Order", route: "/reports/po" },
         ],
       },
-      { name: "Archive", url: "/archive", icon: MdArchive},
+      { name: "Archive", url: "/archive", icon: MdArchive },
+    ],
+    accounting: [
+      { name: "Dashboard", url: "/dashboard", icon: MdDashboard },
+      { name: "Request", url: "/request", icon: MdAssignment },
+      {
+        name: "Inventory",
+        url: "/inventory",
+        icon: MdInventory,
+        spacing: true,
+      },
+      {
+        name: "Reports",
+        icon: MdDescription,
+        isDropdown: true,
+        spacing: true,
+        subItems: [
+          {
+            name: "Delivery Instruction Form",
+            route: "/reports/dif",
+            spacing: true,
+          },
+          { name: "Stock Receiving Report", route: "/reports/wsrr" },
+          { name: "Purchase Order", route: "/reports/po" },
+        ],
+      },
+      { name: "Archive", url: "/archive", icon: MdArchive },
+    ],
+    warehouse: [
+      { name: "Dashboard", url: "/dashboard", icon: MdDashboard },
+      { name: "Inventory", url: "/inventory", icon: MdInventory },
+      { name: "Archive", url: "/archive", icon: MdArchive },
+    ],
+    branch: [
+      { name: "Dashboard", url: "/dashboard", icon: MdDashboard },
+      { name: "Request", url: "/request", icon: MdAssignment },
+      { name: "Archive", url: "/archive", icon: MdArchive },
     ],
   };
 
@@ -84,12 +120,10 @@ const Sidebar = ({ role }) => {
     <div className="h-screen bg-[#105D2B] w-72 flex flex-col justify-between text-white">
       {/* <img src={logo} alt="Logo" className="inline-flex float-left w-2 h-3" /> */}
 
-      <nav className="flex-1">
-        {renderMenu(menuItems[role] || [])}
-      </nav>
+      <nav className="flex-1">{renderMenu(menuItems[role] || [])}</nav>
 
       <div className="mb-6">
-        <a 
+        <a
           href="/signout"
           className="flex items-center px-6 py-4 hover:bg-[#003d1a] transition duration-300 text-white font-medium text-lg gap-10"
         >
