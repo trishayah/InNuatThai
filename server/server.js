12af068cced651e69f88271ab549d01502bf4049
trishiah <152073735+pan-borikat@users.noreply.github.com>
2024-12-18 16:49:51 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken'); // Added jwt module
 
 const app = express();
