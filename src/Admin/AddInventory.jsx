60714fee97e7f0b32189496f9d4d3064b9a8eb5f
dertehwendoh <122952472+dertehwendoh@users.noreply.github.com>
2024-12-24 02:14:42 +0800

    Add files via upload

---
+++ src/Admin/AddInventory.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../../Shared UI/searchBar";
+import AccInfo from "../../Shared UI/accInfo";
+
+function AddInventory() {
+  const navigate = useNavigate();
+  const [inventory, setInventory] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Sample data to be used in place of the API response
+  const sampleData = [
+    {
+      inventoryNo: "001",
+      itemName: "Laptop",
+      category: "Electronics",
+      unitPrice: "24000",
+      stock: "100",
+      dateAdded: "12-24-2023",
+    },
+    {
+      inventoryNo: "002",
+      itemName: "Printer Cartridge",
+      category: "Electronics",
+      unitPrice: "300",
+      stock: "199",
+      dateAdded: "2024-12-02",
+     
+    },
+    {
+      inventoryNo: "003",
+      itemName: "Projector",
+      category: "Electronics",
+      unitPrice: "12000",
+      stock: "50",
+      dateAdded: "2024-12-02",
+      
+    },
+  ];
+
+  // Simulate fetching data
+  useEffect(() => {
+    const fetchinventory = async () => {
+      try {
+        setLoading(true);
+        // Simulating API call by setting the sample data
+        setInventory(sampleData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchinventory();
+  }, []);
+
+  // Handle row click to navigate to the details page
+//   const handleRowClick = (inventory) => {
+//     // Redirect to the inventory details page with the inventory number in the URL
+//     navigate(`/inventory/${inventory.inventoryNo}`);
+//   };
+
+  return (
+    <div className="flex flex-col w-full min-w-screen h-full min-h-screen bg-[#D9D9D9]">
+      <div className="title-container">
+      <h1 className="text-2xl font-500 text-[#133517] font-poppins mt-4 mr-4 p-4">Inventory&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; Add Inventory</h1>
+      </div>
+      <div className="inventoryTable ml-2 mr-2">           
+      <AccInfo />      
+        <div className="search">
+          <SearchBar />       
+          <select style={{ backgroundColor: "#133517", color: "#FFFFFF"}} className="sorting">
+              <option value="">Sort by</option>
+              <option value="asc">A-Z</option>
+              <option value="desc">Z-A</option>
+              <option value="category">Category</option>
+            </select>  
+          <select style={{ backgroundColor: "#133517", color: "#FFFFFF" }} className="option">
+              <option value="update">Update Request</option>
+              <option value="add">Add Request</option>
+              <option value="remove">Remove Request</option>
+              <option value="view">View Inventory</option>
+            </select>
+          </div>
+
+        <table className="inventoryTabletable-fixed border-spacing-2 w-full border-collapse border border-gray-300 font-poppins font-semibold">
+          <thead>
+            <tr style={{ backgroundColor: "#133517", color: "#FFFFFF" }} className="border border-gray-300">
+              <th className="px-4 py-2 text-center">Inventory No.</th>
+              <th className="px-4 py-2 text-center">Item Name</th>
+              <th className="px-4 py-2 text-center">Category</th>
+              <th className="px-4 py-2 text-center">Unit Price</th>
+              <th className="px-4 py-2 text-center">Stock</th>
+              <th className="px-4 py-2 text-center">Date Added</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            {inventory.map((inventory, index) => (
+              <tr
+                // key={inventory.inventoryNo}
+                // onClick={() => handleRowClick(inventory)}
+                style={{
+                  backgroundColor: index % 2 === 0 ? "#FFF5F5" : "#D3DDD6",
+                  cursor: "pointer",
+                }}
+              >
+                <td className="px-4 py-2 border text-center">{inventory.inventoryNo}</td>
+                <td className="px-4 py-2 border text-center">{inventory.itemName}</td>
+                <td className="px-4 py-2 border text-center">{inventory.category}</td>
+                <td className="px-4 py-2 border text-center">{inventory.unitPrice}</td>
+                <td className="px-4 py-2 border text-center">{inventory.stock}</td>
+                <td className="px-4 py-2 border text-center">{inventory.dateAdded}</td>
+
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+    </div>
+  );
+}
+
+export default AddInventory;
---
+++ src/Admin/NextApprovalRequest.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+// import SearchBar from "./searchBar";
+import AccInfo from "../../Shared UI/accInfo";
+
+function NextApprovalRequest() {
+  const navigate = useNavigate();
+  const [requests, setRequests] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+//     // Handle row click to navigate to the details page
+//     const handleRowClick = (request) => {
+//       // Redirect to the request details page with the request number in the URL
+//       navigate(`/request/${request.requestNo}`);
+//     };
+
+return (
+    <div className="flex flex-col w-full min-w-screen h-full min-h-screen bg-[#D9D9D9]">
+      <h1 className="approval text-2xl font-500 text-[#133517] mt-4 font-poppins mr-4 p-4">For Approval</h1>
+      <div className="requestTable">
+        <AccInfo />
+        <div className="reqbox">
+          <h2 className="reqform">Request Form</h2>
+          <h3 className="reqformdetails">Please fill all information on the fields.</h3>
+        <div className='form-container'>
+          <div className="form-row">
+            <div className="form-group">
+              <label htmlFor="date" className="reqdate">Date</label>
+              <input type="date" id="date" className="form-input" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="title" className="reqtitle">Title</label>
+              <input type="text" id="title" className="form-input" />
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-group">
+              <label htmlFor="requester" className="reqman">Requester</label>
+              <input type="text" id="requester" className="form-input" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="branch" className="reqbranch">Branch</label>
+              <input type="text" id="branch" className="form-input" />
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-group">
+              <label htmlFor="receivedBy" className="reqrcv">Received by</label>
+              <select id="receivedBy" className="form-input">
+                <option value="">Select</option>
+                <option value="person1">lulu romualdez</option>
+                <option value="person2">Marcos yawa</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="decisionPersonnel" className="reqdecision">Decision Personnel</label>
+              <select id="decisionPersonnel" className="form-input">
+                <option value="">Select</option>
+                <option value="person1">Beyonce</option>
+                <option value="person2">Shaq</option>
+              </select>
+            </div>
+          </div>
+          </div>
+          <div className="buttonContainer">
+            <button className="button1">Cancel</button>
+            <button className="button3">Decline</button>
+            <button className="button4">Approve</button>
+          </div>
+        </div>
+      </div>
+      {/* {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>} */}
+    </div>
+  );
+}
+
+export default NextApprovalRequest;
